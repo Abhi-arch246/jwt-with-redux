8f30362da0d288e1c1f0a97746c99f9f3dbe236d
Abhi-arch246 <abhishekreddy235@gmail.com>
2022-06-06 22:25:09 +0530

    Backend complete

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
 const app = express()
 const connectDb = require('./db')
-
+const userRoute = require('./routes/userRoute')
 connectDb();
 
+app.use(express.json())
+app.use("/api/users/", userRoute)
 
 app.listen(port, () => console.log(`Server running on ${port}`));
